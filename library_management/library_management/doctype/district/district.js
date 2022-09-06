1b6c5073a07369e846fb0e6b04ef144bd7fd1a1a
Chhun Sok Trylisin <lisin.morakot@gmail.com>
2022-09-06 22:39:22 +0700

    testing

---
+++ library_management/library_management/doctype/district/district.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Lisin and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('District', {
+	
+	// refresh: function(frm) {
+    //     frm.add_custom_button('Create Commune', () => {
+    //         frappe.new_doc('District', {
+    //             District: frm.doc.name
+    //         })
+    //     })
+    // }
+
+});
